5e96c792f4d1ee88b2f039bb104fbd1c171af2c8
Alyssa Vivien S. Cañas <alyssavivien.sc@gmail.com>
2024-03-26 18:15:33 +0800

    1

--- src/Js/Organizer_HomePage.js
+++ src/Js/Organizer_HomePage.js
@@ -21,22 +21,34 @@ function Organizer_HomePage() {
         <div className='orgcard'>
           <h4>Events</h4>
           <ul>
-          {events.map(event => (
-            <div key={event.eventID} className="eventRow">
-              <div className='dateandtime'>
+            {events.map(event => (
+              <div key={event.eventID} className="eventRow">
                 <div className='etitle'>{event.eventTitle}</div>
-                <div className='edate'>{new Date(event.eventDate).toLocaleDateString()}, {event.eventTime.replace(/:[0-9]{2}\.?[0-9]*$/, '')}</div>
+                  <div className='dateandtime'>
+                    <div className='edes'>{event.eventDescription}</div>
+                    <div className='edate'>{new Date(event.eventDate).toLocaleDateString()}, {event.eventTime.replace(/:[0-9]{2}\.?[0-9]*$/, '')}</div>
+                  </div>
               </div>
-              <div className='desbut'>
-                <div className='edes'>{event.eventDescription}</div>
-                <button className='cbutton'>Cancel Event</button>
-              </div>
-            </div>
-          ))}
+            ))}
           </ul>
         </div>
         <div className='orgcard1'>
-          <h4>My Events</h4>  
+          <h4>My Events</h4>
+          <ul>
+            {events.map(event => (
+              <li key={event.eventID} className="eventRow">
+                  <div className='etitle'>{event.eventTitle}</div>
+                  <div className='dateandtime'>
+                    <div className='edes'>{event.eventDescription}</div>
+                    <div className='edate'>{new Date(event.eventDate).toLocaleDateString()}, {event.eventTime.replace(/:[0-9]{2}\.?[0-9]*$/, '')}</div>
+                  </div>
+                  <div className='last'>
+                    <input className='reasoninput' placeholder='Why cancel the event?' />
+                    <button className='ccbutton'>Cancel Event</button>
+                  </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
   );
