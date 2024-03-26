6132c42790c292a51676f8c9db16c800b9c40acb
Alyssa Vivien S. Cañas <alyssavivien.sc@gmail.com>
2024-03-26 17:15:43 +0800

    000

--- src/Js/UserNotifications_Page.js
+++ src/Js/UserNotifications_Page.js
@@ -3,6 +3,28 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function UserNotifications_Page() {
+
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
+
+
+  const fetchNotifications = () => {
+    axios.get('http://localhost:8081/usernotifications', {withCredentials: true})
+      .then(response => {
+        setNotifications(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching notifications:', error);
+      });
+  };
+
+  const getStatusMessage = (status) => {
+    return status === 0 ? "Request to join event is Disapproved" : "Request to join event is Approved";
+  };
+
   /*const [requests, setRequests] = useState([]);
 
     const [values, setValues] = useState({
@@ -36,15 +58,15 @@ function UserNotifications_Page() {
 
   const getStatusMessage = (status) => {
     return status === 0 ? "Request to join event is Disapproved" : "Request to join event is Approved";
-  };
+  };*/
 
   return (
       <div className='reqbody'>
         <div className='reqcard'>
           <h4>Notifications</h4>
-            {/*<ul>
-            {requests.map(request => (
-                <div key={request.requestID} className="reqRow" onClick={() => handleSelectRequest(request)}>
+            <ul>
+              {notifications.map(notification => (
+                <div key={notification.notificationID} className="reqRow">
                     <div className='des'>
                         <div className='etitle'>{notification.eventTitle}</div>
                         <div className='status'>{getStatusMessage(notification.status)}</div>
